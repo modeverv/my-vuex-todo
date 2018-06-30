3ba88bbf59b854838ba329bfaa19cc41b7789405
modeverv <modeverv@gmail.com>
2018-07-01 01:10:22 +0900

    2018-07-01 01:10:22

--- src/main.ts
+++ src/main.ts
@@ -16,3 +16,40 @@ new Vue({
   template: '<App/>'
   
 })
+
+// typescript
+var str1: string = "strict";
+//var str2: number = "aaae";
+var srt3 = "str3";
+
+interface Inta {
+  method(): string;
+}
+class Parent {
+  protected aaa: string;
+  constructor(name: string) {
+    this.aaa = name;
+  }
+  protected hello(word: string): string {
+    return this.aaa + " world";
+  }
+}
+
+class Child extends Parent implements Inta {
+  protected str = 'str';
+
+  constructor(name: string) {
+    super(name);
+  }
+
+  public method(): string {
+    return this.str;
+  }
+
+  public public_hello(): string {
+    return this.method() + this.aaa
+  }
+}
+
+var hoge: Child = new Child("test");
+alert(hoge.public_hello());
