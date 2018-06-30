54af1a4b3c3e66b76c4bdc1b76ffc9d95187436b
modeverv <modeverv@gmail.com>
2018-07-01 04:43:02 +0900

    2018-07-01 04:43:02

---
+++ src/vuex/store.ts
@@ -0,0 +1,25 @@
+import { TASKS } from '@/vuex/types';
+import Vue from 'vue';
+import Vuex from 'vuex';
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+    state: {
+        tasks: ["aaa","bbbb"]
+    },
+    getters: {
+        tasks: function (state) {
+            return state.tasks
+        }
+    },
+    mutations: {
+        [TASKS]: function (state, task) {
+            state.tasks.push(task)
+        }
+    },
+    actions: {
+        addTask: function ({ commit}, task) {
+            commit(TASKS, task)
+        }
+    }
+})
